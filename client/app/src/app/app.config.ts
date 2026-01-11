bfeaaa1981f64bdfda45b5dccaed37a15732f78d
devjoaopr <joaoprsantos1@outlook.com>
2026-01-10 23:52:41 -0300

    Add internationalization support and Tailwind CSS configuration - Added Tailwind CSS configuration file (tailwind.config.js) to enable dark mode and set up content paths.

--- client/app/src/app/app.config.ts
+++ client/app/src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { importProvidersFrom } from '@angular/core';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(TranslateModule.forRoot()),
+    ...provideTranslateHttpLoader({
+      prefix: '/assets/i18n/',
+      suffix: '.json',
+    }),
+  ],
 };
--- client/app/src/app/app.ts
+++ client/app/src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {
+  faBriefcase,
+  faPhone,
+  faLocationDot,
+  faEnvelope,
+  faCode,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class AppComponent {
   protected readonly title = signal('personalsite');
+  faGithub = faGithub;
+  faLocationDot = faLocationDot;
+  faPhone = faPhone;
+  faLinkedin = faLinkedin;
+  faEnvelope = faEnvelope;
+  faCode = faCode;
+  faUser = faUser;
+  faBriefCase = faBriefcase;
 }
